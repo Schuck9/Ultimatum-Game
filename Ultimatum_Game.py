c717133c951ed401d182e4a8b7a92b1c6317e832
Schuck9 <34061849+Schuck9@users.noreply.github.com>
2020-02-09 17:13:15 +0800

    bug fixed

--- Ultimatum_Game.py
+++ Ultimatum_Game.py
@@ -1,21 +1,24 @@
 """
 A simple implementation of Ultimatum Game
-@date: 2020.2.8
+@date: 2020.2.9
 @author: Tingyu Mo
 """
 
 import numpy as np
 import pandas as pd
 import os
+import time
 
 class Ultimatum_Game():
-    def __init__(self,N,w,u,divided_part = 12):
+    def __init__(self,N,w,u,divided_part = 12+1):
         self.N = N
         self.w = w
         self.u = u
         # self.meta_element = np.around(np.arange(0,1,1/12),2) #1/12 equally divided
         self.meta_element=np.arange(divided_part)
         self.Frequency_matrix = np.zeros((divided_part,divided_part))
+        self.dir_str = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+        os.mkdir("./result/{}".format(self.dir_str))
 
     def Make_Player(self,N):
         '''
@@ -41,22 +44,23 @@ class Ultimatum_Game():
         '''
         pay_off_array = np.zeros((N,N)) #shape = (N,N-1)
         for P1 in range(self.N):
-            for P2 in range(P1,self.N):
+            for P2 in range(P1+1,self.N):
                 if P1 != P2:
                     # proposer = P1 , responder = P2
                     offer = p_vector[P1]
                     demand = q_vector[P2]
-                    if offer > demand:
+                    if offer >= demand:
                         # acception
-                        pay_off_array[P1][P2] += 1 - offer 
+                        # 1-offer has be modifed as 12 - offer at 2020/2/9/15:17  
+                        pay_off_array[P1][P2] += 12 - offer 
                         pay_off_array[P2][P1] += offer
 
                     # proposer = P2 , responder = P1
                     offer = p_vector[P2]
                     demand = q_vector[P1]
-                    if offer > demand:
+                    if offer >= demand:
                         # acception
-                        pay_off_array[P2][P1] += 1 - offer 
+                        pay_off_array[P2][P1] += 12 - offer 
                         pay_off_array[P1][P2] += offer   
                 else:
                     continue
@@ -111,13 +115,75 @@ class Ultimatum_Game():
 
     
     def Save(self,pq_array,Epoch):
-        os.mkdir("./result/w{}_ep{}_u{}".format(self.w,Epoch,self.u))
+        '''
+        save frequency matrix and strategy array
+        '''
+        subdir = "./result/{}/w{}_ep{}_u{}".format(self.dir_str,self.w,Epoch,self.u)
+        os.mkdir(subdir)
         df = pd.DataFrame(data = pq_array)
-        df.to_csv('./result/w{}_ep{}_u{}/strategy_w{}_ep{}_u{}.csv'.format(self.w,Epoch,self.u,self.w,Epoch,self.u),index = None)
+        df.to_csv('./result/{}/w{}_ep{}_u{}/strategy_w{}_ep{}_u{}.csv'.format(self.dir_str,self.w,Epoch,self.u,self.w,Epoch,self.u),index = None)
         freq = pd.DataFrame(data = self.Frequency_matrix*1.0/(100*Epoch))
-        freq.to_csv('./result/w{}_ep{}_u{}/frequency_w{}_ep{}_u{}.csv'.format(self.w,Epoch,self.u,self.w,Epoch,self.u),index = None)
+        freq.to_csv('./result/{}/w{}_ep{}_u{}/frequency_w{}_ep{}_u{}.csv'.format(self.dir_str,self.w,Epoch,self.u,self.w,Epoch,self.u),index = None)
         # df = pd.read_csv('./results.csv')
         # print(df)
+
+    def train(self,p_vector,q_vector):
+        '''
+        evolution starts!
+        '''
+        for Epoch in range(1,Epochs):
+            #各博弈方进行最后通牒博弈并计算各自的得益
+                pay_off_array = self.Play(p_vector,q_vector)
+                #计算各博弈方的平均得益和有效得益
+                effective_payoff = self.Pay_off_calculate(pay_off_array,self.w)
+                #演化过程
+                p_vector,q_vector = self.Moran_process(p_vector,q_vector,effective_payoff,self.u)
+                #计算频率
+                self.Frequency_calculate(p_vector,q_vector,Epoch)
+                if Epoch % 50000== 0:
+                    print("Epoch[{}]".format(Epoch))
+                    print("p_vector:\n{}\nq_vector:\n{}".format(p_vector,q_vector))
+                    print("Frequency:\n{}\n".format(self.Frequency_matrix*1.0/(100*Epoch)))
+                # if Epoch % 100000 == 0:
+                    #保留一位有效数字
+                    p_vector = np.around(p_vector,1)
+                    q_vector = np.around(q_vector,1)
+                    print("p_vector:\n{}\nq_vector:\n{}".format(p_vector,q_vector))
+                    pq_array = np.vstack((p_vector,q_vector))
+                    self.Save(pq_array,Epoch)
+
+    def retrain(self,filepath):
+        '''
+        continue evolution from specific check point
+        '''
+        lists = os.listdir(filepath)         
+        lists.sort(key=lambda fn: os.path.getmtime(filepath + "/" + fn)) 
+        result_dir = os.path.join(filepath, lists[-1])      
+        result_list = os.listdir(result_dir)
+    
+        parse_str = result_list[0][:-4].split("_")
+        if parse_str[0] == 'strategy':
+            strategy_path = os.path.join(result_dir,result_list[0])
+            frequency_path = os.path.join(result_dir,result_list[1])
+        else:
+            strategy_path = os.path.join(result_dir,result_list[1])
+            frequency_path = os.path.join(result_dir,result_list[0])
+        w = float(parse_str[1][1:])
+        Epoch = int(parse_str[2][2:])
+        u = float(parse_str[3][1:])
+        
+        strategy = pd.read_csv(strategy_path)
+        frequency = pd.read_csv(frequency_path)
+        pd_array = strategy.values
+        self.Frequency_matrix = (frequency.values)*(100*Epoch)
+        p_vector,q_vector = pd_array[0],pd_array[1]
+        
+        return p_vector, q_vector,w,Epoch+1,u
+
+    
+
+
+    
 
 
 
@@ -129,11 +195,18 @@ if __name__ == '__main__':
     w = np.around(pow(10,2),4)
     u = np.around(pow(10,-1.25),4) #u = 10^(-1.25)
     Epochs = pow(10,7) #演化轮次
+    # check_point = "./result/2020-02-09-17-06-35"
+    check_point = None
     #生成环境env
     UG = Ultimatum_Game(N,w,u)
-    #创建N个博弈方
-    p_vector, q_vector = UG.Make_Player(N)
-    for Epoch in range(1,Epochs):
+    if check_point!= None:
+        p_vector, q_vector,w,Start,u = UG.retrain(check_point)
+    else:
+        Start = 1
+        #创建N个博弈方
+        p_vector, q_vector = UG.Make_Player(N)
+        
+    for Epoch in range(Start,Epochs):
     #各博弈方进行最后通牒博弈并计算各自的得益
         pay_off_array = UG.Play(p_vector,q_vector)
         #计算各博弈方的平均得益和有效得益
@@ -142,7 +215,7 @@ if __name__ == '__main__':
         p_vector,q_vector = UG.Moran_process(p_vector,q_vector,effective_payoff,u)
         #计算频率
         UG.Frequency_calculate(p_vector,q_vector,Epoch)
-        if Epoch % 100000== 0:
+        if Epoch % 200000== 0:
             print("Epoch[{}]".format(Epoch))
             print("p_vector:\n{}\nq_vector:\n{}".format(p_vector,q_vector))
             print("Frequency:\n{}\n".format(UG.Frequency_matrix*1.0/(100*Epoch)))
